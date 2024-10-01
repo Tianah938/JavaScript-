b6d1bd863e843df1367cf1a4c507bcf49022550d
Modupeoluwa Atoyebi <modupeoluwaatoyebi@gmail.com>
2024-10-01 08:56:15 +0100

    initial commit

---
+++ variable.js
@@ -0,0 +1,17 @@
+var school = "lagos state university"
+var name = "John Peter"
+
+
+console.log(school)
+console.log(name)
+
+let course = "JavaScript"
+let age = 54
+
+console.log(course)
+console.log(age)
+
+
+const topic = "variable is our topic"
+
+console.log(topic)
