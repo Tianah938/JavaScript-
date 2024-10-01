e00bfd258e7bfc9a6cedda728f0295c80555da04
Modupeoluwa Atoyebi <modupeoluwaatoyebi@gmail.com>
2024-10-01 09:14:23 +0100

    next commit

---
+++ Primitive variable.js
@@ -0,0 +1,8 @@
+const name="day"
+const day="wednesday"
+console.log(name)
+console.log(day)
+const position=3
+const output="Today is Wednesday. Its is the 3rd day of the week;"
+const sentence="Today is " +  day  +  ". It is the day "  + position  +  " of the week."
+console.log(sentence)
