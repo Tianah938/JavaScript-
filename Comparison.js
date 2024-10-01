edc29357d0eed5b4f33e57391af4fdc6f438f233
Modupeoluwa Atoyebi <modupeoluwaatoyebi@gmail.com>
2024-10-01 09:23:15 +0100

    next commit

---
+++ Comparison.js
@@ -0,0 +1,15 @@
+const number= 9;
+if (number % 2===0) {
+    console.log(number + " is even");
+ } else {console.log(number + " is odd");}
+
+const Number= 7;
+if (Number % 2===1) {
+    console.log(Number + " is odd");
+} else {console.log(Number + " is even");}
+
+
+const nUmber= 8
+if (nUmber % 2===0) {
+    console.log(nUmber + " is even");
+} else {console.log(nUmber + " is odd");}
