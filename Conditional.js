d64b09808a0f6021adda81667cf79c4d88a1dbe6
Modupeoluwa Atoyebi <modupeoluwaatoyebi@gmail.com>
2024-10-01 09:34:18 +0100

    next commit

---
+++ Conditional.js
@@ -0,0 +1,31 @@
+side1 = 8;
+  side2 = 8;
+  side3 = 8
+if(side1===side2 && side1===side3){
+    console.log("Equilateral triangle")
+} else if (side1===side2 || side1===3 || side2===side3){
+    console.log("Isosceles traingle")
+} else {
+    console.log("Scalene traingle")
+}
+
+ side1 = 8;
+  side2 = 5;
+  side3 = 8;
+if(side1===side2 && side1===side3){
+    console.log("Equilateral triangle")
+} else if (side1===side3 || side3===side1){
+    console.log("Isosceles traingle")
+} else {console.log("Scalene traingle")
+}
+
+side1 = 8;
+ side2 = 2;
+ side3 = 6;
+if(side1===side2 && side1===side3){
+    console.log("Equilateral triangle")
+} else if (side1===side3 || side3===side1){
+    console.log("Isosceles traingle")
+} else {
+    console.log("Scalene traingle") 
+}
